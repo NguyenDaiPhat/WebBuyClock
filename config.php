6ce7cd0e506ad6326a3c86a1242f4cc6f5d813de
Lineinuniv <daiphat2442003@gmail.com>
2023-05-04 17:45:12 +0700

    update host

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-$conn = mysqli_connect('database-project.cc8wmiuolpsc.ap-southeast-1.rds.amazonaws.com','admin','12345678','shoponline_db') or die('connection failed');
+$conn = mysqli_connect('databaseproject.cc8wmiuolpsc.ap-southeast-1.rds.amazonaws.com','admin','12345678','shoponline_db') or die('connection failed');
 
 ?>
